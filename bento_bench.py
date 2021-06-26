8f9ce16f13150c267b08ae008e96d798d948339f
Peter-Jan Gootzen <peter-jan@gootzen.net>
2021-06-27 00:23:33 +0200

    Better results processing

--- bento_bench.py
+++ bento_bench.py
@@ -41,3 +41,34 @@ def bar_plot(ax, data, l, yerr=None, colors=None, total_width=0.8, single_width=
     
     if l is not None:
         ax.legend(bars, l)
+
+def set_size(width_pt, fraction=1, subplots=(1, 1)):
+    """Set figure dimensions to sit nicely in our document.
+
+    Parameters
+    ----------
+    width_pt: float
+            Document width in points
+    fraction: float, optional
+            Fraction of the width which you wish the figure to occupy
+    subplots: array-like, optional
+            The number of rows and columns of subplots.
+    Returns
+    -------
+    fig_dim: tuple
+            Dimensions of figure in inches
+    """
+    # Width of figure (in pts)
+    fig_width_pt = width_pt * fraction
+    # Convert from pt to inches
+    inches_per_pt = 1 / 72.27
+
+    # Golden ratio to set aesthetic figure height
+    golden_ratio = (5**.5 - 1) / 2
+
+    # Figure width in inches
+    fig_width_in = fig_width_pt * inches_per_pt
+    # Figure height in inches
+    fig_height_in = fig_width_in * golden_ratio * (subplots[0] / subplots[1])
+
+    return (fig_width_in, fig_height_in)
